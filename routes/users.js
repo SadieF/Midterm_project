013f012ae6149a9cf19f06614d5b89052fce0815
Anhad Gill <gillanhad23@gmail.com>
2018-04-05 19:45:51 +0000

    Adds a function that inserts form data to the database

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,18 @@
 
 const express = require('express');
 const router  = express.Router();
+const pg = require("pg");
+
+require('dotenv').config();
+
+db.connect({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  db: process.env.DB_NAME,
+  ssl: process.env.DB_SSL,
+  port: process.env.DB_PORT
+});
 
 module.exports = (knex) => {
 
@@ -13,6 +25,16 @@ module.exports = (knex) => {
         res.json(results);
     });
   });
+
+  router.post('/', (res, req) => {
+    /*
+    Logic:
+    1. takes the data(first_name, last_name, email) in req.body
+        - 
+    2. adds it to the database
+    3. redirects to thank you page
+    */
+  })
 
   return router;
 }
