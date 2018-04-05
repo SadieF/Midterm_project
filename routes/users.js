667f5b7d9573fa5e9fab752a4905596332aaa126
Anhad Gill <gillanhad23@gmail.com>
2018-04-05 21:59:13 +0000

    Adds more insert queries

--- routes/users.js
+++ routes/users.js
@@ -16,6 +16,72 @@ db.connect({
 });
 
 module.exports = (knex) => {
+  let templateVars = {
+    email: req.body.email,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    title: req.body.title,
+    option_1: req.body.option_1,
+    option_1_desc: req.body.option_1_desc,
+    option_2: req.body.option_2,
+    option_2_desc: req.body.option_2_desc,
+    option_3: req.body.option_3,
+    option_3_desc: req.body.option_3_desc,
+    option_4: req.body.option_4,
+    option_4_desc: req.body.option_4_desc,
+    option_5: req.body.option_5,
+    option_5_desc: req.body.option_1_desc
+  };
+
+  // function insertBio (tempObject) {
+  //   knex('admins').insert(templateVars.email, templateVars.firstName, templateVars.lastName)
+  //     asCallback ((err, results) => {
+  //       if (err) {
+  //         console.log("Error from insert", err);
+  //       }
+  //       knex.select('*').from('admins').asCallback((error, rows) => {
+  //         if (error) {
+  //           console.log('Error from select', error);
+  //         }
+  //         console.log('rows: ', rows);
+  //       })
+  //     })
+  // };
+
+  // Creates a random alphanumeric string
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 10);
+}
+
+  // insert using promises
+  function insertData(tempObject) {
+    const adminRandomKey = generateRandomString();
+    const shareRandomKey = generateRandomString();
+
+
+    const insertBioPromise = knex('admins')
+    .insert(templateVars.email, templateVars.firstName, templateVars.lastName);
+
+    const insertedBioPromise = insertBioPromise
+    .then((admin) => {
+      console.log('Here are my admins:', things);
+    })
+
+    const insertPollsPromise = knex('polls')
+    .insert(adminURL_random_key, shareURL_random_key, name)
+
+
+
+
+    // .catch((err) => {
+    //   console.log('Err', err);
+    // });
+  }
+
+
+  function insertData (tempObject) {
+    //use today's lecture to add stuff
+  }
 
   router.get("/", (req, res) => {
     knex
