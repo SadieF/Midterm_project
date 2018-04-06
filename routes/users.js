b818f847ddbecc4689c1e68860992ebf856197a5
Anhad Gill <gillanhad23@gmail.com>
2018-04-06 20:59:31 +0000

    Inserting form data into the database queries is complete

--- routes/users.js
+++ routes/users.js
@@ -13,83 +13,51 @@ function generateRandomString() {
 module.exports = (knex) => {
   console.log('I GOT HERE 1');
 
-  // let templateVars = {
-  //   email: req.body.email,
-  //   firstname: req.body.firstname,
-  //   lastname: req.body.lastname,
-  //   title: req.body.title,
-  //   option_1: req.body.option_1,
-  //   option_1_desc: req.body.option_1_desc,
-  //   option_2: req.body.option_2,
-  //   option_2_desc: req.body.option_2_desc,
-  //   option_3: req.body.option_3,
-  //   option_3_desc: req.body.option_3_desc,
-  //   option_4: req.body.option_4,
-  //   option_4_desc: req.body.option_4_desc,
-  //   option_5: req.body.option_5,
-  //   option_5_desc: req.body.option_1_desc
-  // };
-
-  let templateVars = {
-    email: 'a@a',
-    firstname: 'Anhad',
-    lastname: 'Gill',
-    title: 'Sports',
-    option_1: 'Soccer',
-    option_1_desc: 'It\'s a game played with a feet and a ball',
-    option_2: 'Cricket',
-    option_2_desc: 'It\'s a game played with a bat and a ball',
-    // option_3: req.body.option_3,
-    // option_3_desc: req.body.option_3_desc,
-    // option_4: req.body.option_4,
-    // option_4_desc: req.body.option_4_desc,
-    // option_5: req.body.option_5,
-    // option_5_desc: req.body.option_1_desc
-  };
-
   // insert using promises
   function insertData(tempObject) {
     const adminRandomKey = generateRandomString();
     const shareRandomKey = generateRandomString();
 
-    //const insertInfoPromise = knex(nameOfTable).insert([{email:templateVars.email}, {firstname: templateVars.firstName}, {lastname: templateVars.lastName}]);
-
-
-      knex('admin')
+    knex('admin')
+    .insert({
+      email: req.body.email,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname
+    })
+    .returning('id')
+    .then((id) => {
+      console.log('Here are my id for admins:', id);
+      // of course I only want the first result
+      id = id[0];
+      return knex('polls')
       .insert({
-        email:templateVars.email,
-        firstname: templateVars.firstName,
-        lastname: templateVars.lastName
+        admin_id: id,
+        adminurl_random_key: adminRandomKey,
+        shareurl_random_key: shareRandomKey,
+        name: 'req.body.title'
       })
       .returning('id')
-      .then((id)=>{
-        console.log('Here are my id for admins:', id);
-        return knex('polls')
-        .insert(
-          {admin_id: id},
-          {adminurl_random_key: adminRandomKey},
-          {shareurl_random_key: shareRandomKey},
-          {name: templateVars.title})
-        })
-        .returning('id')
-        .then((id) => {
+      })
+      .then((id) => Number(id[0]))
+      .then((id) => {
         console.log('Here are my poll_id:', id);
-        })
-      // .then((poll_id) => {
-      //   knex('options').insert([{option: templateVars.option_1}, {option_1_desc: templateVars.option_1_desc}, {order: 1}])
-      //   knex('options').insert([{option: templateVars.option_2}, {option_2_desc: templateVars.option_2_desc}, {order: 2}])
-      //   knex('options').insert([{option: templateVars.option_3}, {option_3_desc: templateVars.option_3_desc}, {order: 3}])
-      //   knex('options').insert([{option: templateVars.option_4}, {option_4_desc: templateVars.option_4_desc}, {order: 4}])
-      //   knex('options').insert([{option: templateVars.option_5}, {option_5_desc: templateVars.option_5_desc}, {order: 5}])
-      //   return poll_id;
-      // })
+        return (knex('options')
+        .insert([
+        ({polls_id : id, option: templateVars.option_1, option_desc: templateVars.option_1_desc, "order": 1}),
+        ({polls_id : id, option: templateVars.option_2, option_desc: templateVars.option_2_desc, "order": 2}),
+        ({polls_id : id, option: templateVars.option_3, option_desc: templateVars.option_3_desc, "order": 3}),
+        ({polls_id : id, option: templateVars.option_4, option_desc: templateVars.option_4_desc, "order": 4}),
+        ({polls_id : id, option: templateVars.option_5, option_desc: templateVars.option_5_desc, "order": 5})
+      ])
+    )})
       .catch((err) => {
         console.log("err", err);
       })
-      console.log('I GOT HERE');
-    return insertDataPromise;
+      console.log('FUNCTION ENDED');
+  //return insertDataPromise;
   }
 
+  //root directory
   router.get("/", (req, res) => {
     knex
     .select("*")
@@ -99,8 +67,35 @@ module.exports = (knex) => {
     });
   });
 
+  //testing route which can be deleted later
+  router.get('/test', (req, res) => {
+    //insertData(templateVars);
+  });
+
+  //POST - route that adds form data to the database and redirects to the thank you page
   router.post('/', (req, res) => {
+    let templateVars = {
+      email: req.body.email,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      title: req.body.title,
+      option_1: req.body.option_1,
+      option_1_desc: req.body.option_1_desc,
+      option_2: req.body.option_2,
+      option_2_desc: req.body.option_2_desc,
+      option_3: req.body.option_3,
+      option_3_desc: req.body.option_3_desc,
+      option_4: req.body.option_4,
+      option_4_desc: req.body.option_4_desc,
+      option_5: req.body.option_5,
+      option_5_desc: req.body.option_5_desc
+    };
+    //insertData(templateVars);
     res.redirect('/thankyou')
+  })
+
+  router.get('/thanks', (req, res) => {
+    res.render('thanks.ejs')
   })
 
   return router;
